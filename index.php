11dda9942eee615e969945514b0a14170b5f9336
panncho <francisco.castilloh@usm.cl>
2022-12-14 17:00:14 -0300

    first commit

---
+++ index.php
@@ -0,0 +1,26 @@
+<?php
+
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <title> fc Pictures </title>
+
+    <link rel="stylesheet" href="styles/normalize.css">
+    <link rel="stylesheet" href="styles/main.css?v=<?php echo time(); ?>">
+
+    <link rel="preconnect" href="https://fonts.googleapis.com"><link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Inconsolata:wght@400;700&family=La+Belle+Aurore&display=swap" rel="stylesheet">
+</head>
+<body>
+    <?php include("header.php"); ?>
+    
+</body>
+</html>
